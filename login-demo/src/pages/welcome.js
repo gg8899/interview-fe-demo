f37e91e644cad3bea4f27dc2dba596e00130fec4
GUOKKK <528032175@qq.com>
2024-09-27 19:38:21 +0800

    优化

--- login-demo/src/pages/welcome.js
+++ login-demo/src/pages/welcome.js
@@ -1,8 +1,17 @@
 
-import { useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
-export default () => {
+const Component = () => {
     const state = useLocation()
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        if (!state?.state?.username) {
+            navigate('/login')
+        }
+    }, [])
     return <>Welcome！！！{state?.state?.username}</>
 }
+
+export default Component
